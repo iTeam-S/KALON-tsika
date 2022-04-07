45c9452836bef314a8ef041f168175e0d9b1f522
joseeange04 <ravololonirinap19.aps1a@gmail.com>
2022-04-07 05:02:23 +0300

    Fetching data

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 import ampalibe
 from conf import Configuration
 from ampalibe import Payload
-from test import Requete
+from requete import Requete
 
 bot = ampalibe.init(Configuration())
 chat = bot.chat
@@ -18,13 +18,42 @@ def main(sender_id, cmd, **extends):
         {
             "content_type": "text",
             "title": 'Rechercher 🎼',
-            "payload": Payload("Rechercher")
+            "payload": "RECHERCHER"
         },
         {
             "content_type": "text",
             "title": 'Voir les playlists🎶',
-            "payload": Payload("Listes")
+            "payload": "LISTES"
         }
     ]
     chat.send_quick_reply(sender_id, quick_rep, 'Que souhaitez-vous faire?')
 
+
+
+def Music_lists(self):
+    #----------------------------*$*---------------------------------------#
+    #                    Fetching all data from our database        
+    #-----------------------------*$*--------------------------------------#
+    self.data = req.list_music()
+    musics=[]
+    i=0
+    while i<len(self.data):
+        musics.append({
+            "title":str(self.data[i][0]+ "-" + self.data.[i][1]),
+            "image_url":self.data[i][2],
+            "subtitle":"Artiste:" + str(self.data[i][3]),
+            "buttons":[
+                {
+                    "type":"postback",
+                    "title":"Regarder et écouter",
+                    "payload":"VOIR"+" "+str(self.data[i][0])
+                },
+                {
+                    "type":"postback",
+                    "title":"Télécharger",
+                    "payload":"DOWNLOAD"+" "+str(self.data[i][0])
+                }
+            ]
+        })
+        i+=1
+    return musics
